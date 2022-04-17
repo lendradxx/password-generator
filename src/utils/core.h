095eeae3740b652d5b7d2fca0841507272bfdf50
Lendra <lendev0406@gmail.com>
2022-04-17 12:05:54 +0700

    Update core.h

--- src/utils/core.h
+++ src/utils/core.h
@@ -2,9 +2,17 @@
 #define CORE_H
 
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void visitUrl(char* url) {
     gtk_show_uri_on_window(NULL, url, time(0), NULL);
+}
+
+int randomNumber(int minimum, int maximum) {
+    srand(time(0));
+    return (rand() % (maximum - minimum)) + 1;
 }
 
 #endif
